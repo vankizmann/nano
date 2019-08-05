784fb2cc81dcd4d1405d2cfc3437d95be0d17808
Eduard Kizmann <kizmann@protonmail.ch>
2019-08-05 16:10:57 +0200

    *

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let scope = window || this;
+
 let UUID = require('uuid/v1');
 export { UUID };
 
@@ -7,8 +9,14 @@ export * from "./library/utility";
 import Component from "./library/component";
 export * from "./library/component";
 
+export const Nano = scope.Nano = {
+    ...Utility, ...Component, extend: (extend) => {
+        Nano.Any.keys(extend).forEach((key) => scope.Nano[key] = extend[key]);
+    }
+};
+
 export function NanoInstall(obj) {
-    Nano.Any.keys(Nano).forEach((key) => obj[key] = Nano[key]);
+    Nano.Any.keys(scope.Nano).forEach((key) => obj[key] = scope.Nano[key]);
 }
 
 export function VueGlobal (callback) {
@@ -25,14 +33,14 @@ export function VueNano (Vue) {
     Vue.UUID = UUID;
     Vue.prototype.UUID = UUID;
 
-    Vue.ready = Nano.Dom.ready;
-    Vue.prototype.ready = Nano.Dom.ready;
+    Vue.ready = scope.Nano.Dom.ready;
+    Vue.prototype.ready = scope.Nano.Dom.ready;
 
-    Vue.trans = Nano.Locale.trans;
-    Vue.prototype.trans = Nano.Locale.trans;
+    Vue.trans = scope.Nano.Locale.trans;
+    Vue.prototype.trans = scope.Nano.Locale.trans;
 
-    Vue.choice = Nano.Locale.choice;
-    Vue.prototype.choice = Nano.Locale.choice;
+    Vue.choice = scope.Nano.Locale.choice;
+    Vue.prototype.choice = scope.Nano.Locale.choice;
 
     require("./vue/bootstrap");
     require("./pro/bootstrap");
@@ -40,14 +48,11 @@ export function VueNano (Vue) {
     return Vue;
 }
 
-export const Nano = {
-    ...Utility, ...Component, extend: (extend) => Nano.Obj.assign(Nano, extend)
-};
-
-if ( window !== undefined ) {
-    NanoInstall(window.Nano = {});
+export function extend (extend) {
+    console.log(scope.Nano, extend);
+    Nano.Obj.assign(scope.Nano, extend);
 }
 
 VueGlobal(VueNano);
 
-export default Nano;
+export default scope.Nano;
