c7a2fae19536dd2ee5ab462f6191ad6fe7d914cf
Eduard Kizmann <kizmann@protonmail.ch>
2019-08-06 17:25:43 +0200

    *

--- src/index.js
+++ src/index.js
@@ -1,74 +1,56 @@
 
-import Arr from "./library/utility/array";
-import Obj from "./library/utility/object";
-import Num from "./library/utility/number";
-import Str from "./library/utility/string";
-import Any from "./library/utility/any";
-import Dom from "./library/utility/dom";
-import Ajax from "./library/component/ajax";
-import Asset from "./library/component/asset";
-import Auth from "./library/component/auth";
-import Data from "./library/component/data";
-import Element from "./library/component/element";
-import Event from "./library/component/event";
-import Extension from "./library/component/extension";
-import Locale from "./library/component/locale";
-import Queue from "./library/component/queue";
-import Route from "./library/component/route";
+import ArrSource from "./library/utility/array";
+import ObjSource from "./library/utility/object";
+import NumSource from "./library/utility/number";
+import StrSource from "./library/utility/string";
+import AnySource from "./library/utility/any";
+import DomSource from "./library/utility/dom";
+import AjaxSource from "./library/component/ajax";
+import AssetSource from "./library/component/asset";
+import AuthSource from "./library/component/auth";
+import DataSource from "./library/component/data";
+import ElementSource from "./library/component/element";
+import EventSource from "./library/component/event";
+import ExtensionSource from "./library/component/extension";
+import LocaleSource from "./library/component/locale";
+import QueueSource from "./library/component/queue";
+import RouteSource from "./library/component/route";
 
 let UUID = require('uuid/v1');
-let Cookie = require('js-cookie');
+export { UUID };
 
-export {
-    Arr,
-    Obj,
-    Num,
-    Str,
-    Any,
-    Dom,
-    Ajax,
-    Asset,
-    Auth,
-    Data,
-    Element,
-    Event,
-    Extension,
-    Locale,
-    Queue,
-    Route,
-    Cookie,
-    UUID
-};
+let Cookie = require('js-cookie');
+export { Cookie };
 
 export function NanoInstance() {
 
     this.UUID = UUID;
-
-    this.Arr = Arr;
-    this.Obj =  Obj;
-    this.Num =  Num;
-    this.Str =  Str;
-    this.Any =  Any;
-    this.Dom =  Dom;
-
-    this.Ajax =  Ajax;
-    this.Asset =  Asset;
-    this.Auth =  Auth;
-    this.Data =  Data;
-    this.Element =  Element;
-    this.Event =  Event;
-    this.Extension =  Extension;
-    this.Locale =  Locale;
-    this.Queue =  Queue;
-    this.Route =  Route;
     this.Cookie =  Cookie;
 
+    this.Arr = ArrSource;
+    this.Obj =  ObjSource;
+    this.Num =  NumSource;
+    this.Str =  StrSource;
+    this.Any =  AnySource;
+    this.Dom =  DomSource;
+
+    this.Ajax =  AjaxSource;
+    this.Asset =  AssetSource;
+    this.Auth =  AuthSource;
+    this.Data =  DataSource;
+    this.Element =  ElementSource;
+    this.Event =  EventSource;
+    this.Extension =  ExtensionSource;
+    this.Locale =  LocaleSource;
+    this.Queue =  QueueSource;
+    this.Route =  RouteSource;
+
     this.extends = (extend) => {
-        Any.keys(extend).forEach((key) => this[key] = extend[key]);
+        AnySource.keys(extend).forEach((key) => this[key] = extend[key]);
     };
 
     this.install = (target) => {
-        Any.keys(this).forEach((key) => target[key] = this[key]);
+        AnySource.keys(this).forEach((key) => target[key] = this[key]);
     };
 
     return this;
@@ -121,5 +103,23 @@ if ( typeof scope.Nano === 'undefined' ) {
 if ( typeof scope.Vue !== 'undefined' ) {
     VueGlobal(VueNano);
 }
+
+export const Arr = scope.Nano.Arr;
+export const Obj = scope.Nano.Obj;
+export const Num = scope.Nano.Num;
+export const Str = scope.Nano.Str;
+export const Any = scope.Nano.Any;
+export const Dom = scope.Nano.Dom;
+
+export const Ajax = scope.Nano.Ajax;
+export const Asset = scope.Nano.Asset;
+export const Auth = scope.Nano.Auth;
+export const Data = scope.Nano.Data;
+export const Element = scope.Nano.Element;
+export const Event = scope.Nano.Event;
+export const Extension = scope.Nano.Extension;
+export const Locale = scope.Nano.Locale;
+export const Queue = scope.Nano.Queue;
+export const Route = scope.Nano.Route;
 
 export default scope.Nano;
