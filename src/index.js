06355f479839789657119d04094a161b83fdb5f1
Eduard Kizmann <kizmann@protonmail.ch>
2019-08-06 16:47:37 +0200

    *

--- src/index.js
+++ src/index.js
@@ -1,54 +1,42 @@
 
 import Arr from "./library/source/utility/array";
-export Arr from "./library/source/utility/array";
-
 import Obj from "./library/source/utility/object";
-export Obj from "./library/source/utility/object";
-
 import Num from "./library/source/utility/number";
-export Num from "./library/source/utility/number";
-
 import Str from "./library/source/utility/string";
-export Str from "./library/source/utility/string";
-
 import Any from "./library/source/utility/any";
-export Any from "./library/source/utility/any";
-
 import Dom from "./library/source/utility/dom";
-export Dom from "./library/source/utility/dom";
-
 import Ajax from "./library/source/component/ajax";
-export Ajax from "./library/source/component/ajax";
-
 import Asset from "./library/source/component/asset";
-export Asset from "./library/source/component/asset";
-
 import Auth from "./library/source/component/auth";
-export Auth from "./library/source/component/auth";
-
 import Data from "./library/source/component/data";
-export Data from "./library/source/component/data";
-
 import Element from "./library/source/component/element";
-export Element from "./library/source/component/element";
-
 import Event from "./library/source/component/event";
-export Event from "./library/source/component/event";
-
 import Extension from "./library/source/component/extension";
-export Extension from "./library/source/component/extension";
-
 import Locale from "./library/source/component/locale";
-export Locale from "./library/source/component/locale";
-
 import Queue from "./library/source/component/queue";
-export Queue from "./library/source/component/queue";
-
 import Route from "./library/source/component/route";
-export Route from "./library/source/component/route";
 
 let UUID = require('uuid/v1');
-export { UUID };
+
+export {
+    Arr,
+    Obj,
+    Num,
+    Str,
+    Any,
+    Dom,
+    Ajax,
+    Asset,
+    Auth,
+    Data,
+    Element,
+    Event,
+    Extension,
+    Locale,
+    Queue,
+    Route,
+    UUID
+};
 
 export function NanoInstance() {
 
@@ -122,8 +110,6 @@ if (typeof window !== 'undefined') {
 }
 
 export const Nano = new NanoInstance(scope.Nano);
-
-console.log(Nano);
 
 if ( typeof scope.Nano === 'undefined' ) {
     scope.Nano = Nano;
