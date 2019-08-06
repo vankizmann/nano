4a3fe4486cfb1e8df452b6b43ca60f097e457032
Eduard Kizmann <kizmann@protonmail.ch>
2019-08-06 16:07:46 +0200

    *

--- src/index.js
+++ src/index.js
@@ -1,62 +1,16 @@
-let scope = window || this;
-
-let UUID = require('uuid/v1');
-
-import Arr from "./library/extend/utility/array";
-import Obj from "./library/extend/utility/object";
-import Num from "./library/extend/utility/number";
-import Str from "./library/extend/utility/string";
-import Any from "./library/extend/utility/any";
-import Dom from "./library/extend/utility/dom";
-import Ajax from "./library/extend/component/ajax";
-import Asset from "./library/extend/component/asset";
-import Auth from "./library/extend/component/auth";
-import Data from "./library/extend/component/data";
-import Element from "./library/extend/component/element";
-import Event from "./library/extend/component/event";
-import Extension from "./library/extend/component/extension";
-import Locale from "./library/extend/component/locale";
-import Queue from "./library/extend/component/queue";
-import Route from "./library/extend/component/route";
-
-export {
-    Arr, Obj, Num, Str, Any, Dom, Ajax, Asset, Auth, Data, Element, Event, Extension, Locale, Queue, Route, UUID
-};
-
-let extend = (extend) => {
-
-    scope.Nano.Any.keys(extend).forEach((key) => key !== 'extend' ?
-        Nano[key] = scope.Nano[key] = extend[key] : null);
-
-    VueGlobal(VueNano);
-};
-
-let Nano = {
-    Arr, Obj, Num, Str, Any, Dom, Ajax, Asset, Auth, Data, Element, Event, Extension, Locale, Queue, Route, extend
-};
-
-export { Nano };
-
-export function NanoInstall(obj) {
-    Nano.Any.keys(Nano).forEach((key) => key !== 'extend' ?
-        obj[key] = Nano[key] : null);
-}
-
-NanoInstall(scope.Nano = {});
+import Nano from "./library/nano";
+export * from "./library/nano";
 
 export function VueGlobal (callback) {
-    if ( window !== undefined && window.Vue ) {
-        window.Vue.use(callback);
+    if ( scope !== undefined && scope.Vue ) {
+        callback.call(scope, scope.Vue);
     }
 }
 
 export function VueNano (Vue) {
 
-    NanoInstall(Vue);
-    NanoInstall(Vue.prototype);
-
-    Vue.UUID = UUID;
-    Vue.prototype.UUID = UUID;
+    scope.Nano.install(Vue);
+    scope.Nano.install(Vue.prototype);
 
     Vue.ready = scope.Nano.Dom.ready;
     Vue.prototype.ready = scope.Nano.Dom.ready;
@@ -73,6 +27,20 @@ export function VueNano (Vue) {
     return Vue;
 }
 
-VueGlobal(VueNano);
+let scope = {};
+
+if (typeof global !== 'undefined') {
+    scope = global;
+}
+
+if (typeof window !== 'undefined') {
+    scope = window;
+}
+
+scope.Nano = new Nano(scope.Nano);
+
+if ( typeof scope.Vue !== 'undefined' ) {
+    scope.Vue.use(VueNano);
+}
 
 export default scope.Nano;
