6dc964efb8c2be8e14471a0abf0aeee01033f1a7
Eduard Kizmann <kizmann@protonmail.ch>
2019-08-24 01:34:40 +0200

    *

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let jsExport = {
     mode: "development",
-    entry: ["@babel/polyfill", "./src/index.js"],
+    entry: ["./src/index.js"],
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, "dist"),
