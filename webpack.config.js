133400e4fcd82564969709da1b6cc36cb66e44e6
Eduard Kizmann <kizmann@protonmail.ch>
2019-08-26 11:41:09 +0200

    *

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-let jsExport = {
+let jsExtExport = {
     mode: "development",
     entry: ["./src/index.js"],
+    output: {
+        filename: "index.esm.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "Nano",
+        libraryTarget: "umd",
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                include: [
+                    path.resolve(__dirname, './src')
+                ],
+                loader: 'babel-loader',
+                options: {
+                    configFile: path.resolve('./babel.config.js')
+                },
+            }
+        ]
+    },
+    externals: {
+        vue: {
+            root: 'Vue', commonjs: 'vue', commonjs2: 'vue', amd: 'vue'
+        }
+    },
+
+};
+
+let jsWinExport = {
+    mode: "development",
+    entry: ["@babel/polyfill", "./src/index.js"],
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, "dist"),
@@ -59,4 +90,4 @@ let cssExport = {
     ]
 };
 
-module.exports = [jsExport, cssExport];
+module.exports = [jsWinExport, jsExtExport, cssExport];
