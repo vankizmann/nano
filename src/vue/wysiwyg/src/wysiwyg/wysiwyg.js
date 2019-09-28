f98cbbdfc07feb907b131f943c27ec70c85a56dd
Eduard Kizmann <kizmann@protonmail.ch>
2019-09-28 18:37:55 +0200

    *

--- src/vue/wysiwyg/src/wysiwyg/wysiwyg.js
+++ src/vue/wysiwyg/src/wysiwyg/wysiwyg.js
@@ -1,4 +1,11 @@
+import Undo from "undo.js";
+window.Undo = Undo;
+
 import Medium from "medium.js";
+window.Medium = Medium;
+
+import { Nano } from "../../../../index";
+let { Dom } = Nano;
 
 export default {
 
@@ -18,37 +25,52 @@ export default {
 
     mounted()
     {
-        this.medium = new Medium({
-            element: this.$refs.editor,
-            mode: Medium.richMode
-        });
-
-        this.$refs.editor.highlight = () => {
-            if (document.activeElement !== this.$refs.editor) {
-                this.medium.select();
-            }
+        let config = {
+            element: this.$refs.editor
         };
+
+        // config.beforeInvokeElement = () => {
+        //         //     console.log(this.Medium.value());
+        //         // };
+        //         //
+        //         // config.beforeInsertHtml = () => {
+        //         //     console.log(this.Medium.value());
+        //         // };
+
+        this.Medium = new Medium(config);
     },
+
+    methods: {
+
+        bold()
+        {
+            let selection = window.getSelection(), node = selection.baseNode;
+
+            // if ( Dom.find(node).is('p') ) {
+            //     this.Medium.utils.selectNode(node);
+            // }
+
+            if ( Dom.find(node).closest('strong') ) {
+                this.Medium.utils.selectNode(Dom.find(node).closest('strong'));
+            }
+
+            this.Medium.invokeElement('strong', {
+                // className: ['test']
+            });
+        }
+
+    },
+
 
     render()
     {
 
-        let test = () => {
-
-            this.$refs.editor.highlight();
-
-            this.medium.invokeElement('b', {
-                title: "I'm bold!",
-                style: "color: #66d9ef"
-            });
-
-            console.log(this.medium);
-        };
-
         return (
             <div>
-                <NButton vOn:mousedown={test}>Run test</NButton>
-                <div ref="editor"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium animi aut consequuntur culpa dicta dignissimos fugit nisi possimus reiciendis voluptates.</p></div>
+                <NButton vOn:mousedown={this.bold}>Run test</NButton>
+                <div ref="editor" domPropsInnerHTML={this.value} style="min-height: 100px; background: #eee;">
+
+                </div>
             </div>
         );
     }
